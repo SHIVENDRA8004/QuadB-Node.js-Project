9fe171fd20e5c05823bf8bfca02a8946f67d441b
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-14 02:19:44 +0530

    Data fetching : Fetching data from api, converting it into json formate, and mapping through each ticker and seting only required data

--- controllers/fetchAndSortTickers.js
+++ controllers/fetchAndSortTickers.js
@@ -1,5 +1,15 @@
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const fetchAndSortTickers = async () => {
-    const tickers = await fetch("https://api.wazirx.com/api/v2/tickers");
+    const response = await fetch("https://api.wazirx.com/api/v2/tickers");
+    const tickers = await response.json();
+    const data = Object.entries(tickers).map(([_, ticker]) => ({
+        storeName: ticker.name,
+        last: ticker.last,
+        buy: ticker.buy,
+        sell: ticker.sell,
+        volume: ticker.volume,
+        baseUnit: ticker.base_unit,
+    }));
+    console.log(data);
 };
 module.exports = fetchAndSortTickers;
