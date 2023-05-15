44a17cb2a4af394b74843328bfa6ce4691c66bd0
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-16 02:23:24 +0530

    Fixing error

--- controllers/tickers.controller.js
+++ controllers/tickers.controller.js
@@ -3,7 +3,7 @@ const tickerModel = require("../models/tickers.model");
 // Controller that returns all the tickers stored in database
 const getAlltickers = async (req, res) => {
     try {
-        const tickers = await tickerModel.find({});
+        const tickers = await tickerModel.find();
         if (tickers.length === 0) {
             res.json("There are no tickers in database");
         } else {
