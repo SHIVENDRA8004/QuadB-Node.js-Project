565b4b519e59118418194a030b742bcc4826a6bf
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-15 10:10:24 +0530

    Done: Sub Header

--- client/src/components/SubHeader.js
+++ client/src/components/SubHeader.js
@@ -1,7 +1,31 @@
 const SubHeader = () => {
     return (
-        <div>
-            <h1>SubHeader</h1>
+        <div classname="container-fluid">
+            <div className="row mt-4">
+                <div className="col-12 fs-2 d-flex justify-content-center text-secondary ">Best Price to Trade</div>
+            </div>
+            <div className="row d-flex align-items-center mt-1">
+                <div className="col-2 text-center p-0">
+                    <div className="fs-sm-1 text-sky">0.86 %</div>
+                    <div className="text-secondary fw-sm-bold">5 Mins</div>
+                </div>
+                <div className="col-2 text-center p-0">
+                    <div className="fs-sm-1  text-sky">1.66 %</div>
+                    <div className="text-secondary fw-sm-bold">1 Hour</div>
+                </div>
+                <div className="col-4 text-center p-0">
+                    <div className="fs-sm-2 fs-2">₹ 25,22,580</div>
+                    <div className="text-secondary fw-sm-bold">Average BTC/INR net price including commission</div>
+                </div>
+                <div className="col-2 text-center p-0">
+                    <div className="fs-sm-1 text-sky">10.95 %</div>
+                    <div className="text-secondary fw-sm-bold">1 Day</div>
+                </div>
+                <div className="col-2 text-center p-0">
+                    <div className="fs-sm-1 text-sky">14.68 %</div>
+                    <div className="text-secondary fw-sm-bold">7 Day</div>
+                </div>
+            </div>
         </div>
     );
 };
