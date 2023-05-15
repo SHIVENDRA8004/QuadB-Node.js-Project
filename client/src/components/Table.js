e5f0f052c2d0f88d4ad1e60391462781ff72b483
SHIVENDRA8004 <jat13632@gmail.com>
2023-05-15 23:28:46 +0530

    Frontend: Table almost done

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,28 +1,49 @@
+import { useState } from "react";
+import axios from "axios";
 const Table = () => {
+    // const [tickers, setTickers] = useState([]);
+
+    // const fetchData = async () => {
+    //     try {
+    //         const response = await axios.get();
+    //         setTickers(response.data);
+    //         console.log(tickers);
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // };
+    // fetchData();
     return (
-        <div>
-            <table className="table text-light text-center mt-4">
-                <thead className="text-secondary">
-                    <tr>
-                        <th>#</th>
-                        <th>Platform</th>
-                        <th>Last Traded Price</th>
-                        <th>Buy / Sell Price</th>
-                        <th>Difference</th>
-                        <th>Savings</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="container-fluid">
+            <div className="row">
+                <table className="table text-light text-center mt-4">
+                    <thead className="text-secondary">
+                        <tr>
+                            <th>#</th>
+                            <th>Platform</th>
+                            <th>Last Traded Price</th>
+                            <th>Buy / Sell Price</th>
+                            <th>Difference</th>
+                            <th>Savings</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <hr />
+            <div className="row">
+                <div className="col-8 col-sm-6">Copyright &copy; 2019 &nbsp; HoldInfo.com </div>
+                <div className="col-4 col-sm-6 text-end ">Support</div>
+            </div>
         </div>
     );
 };
